a48fe6fa53188151e2fabe1e2f2a5d4090fb8288
Rahul Pradhan <rahulpradhan279@gmail.com>
2018-08-22 11:21:10 -0800

    Assignment 2 Rahul Pradhan

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function makecacheMAtrix does the following ; sets and gets matrix ,sets and gets inverse matrix
+## the purpose of using cached data is to reduce time in calculating the inverse of the same matrix again
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i<-NULL
+  set<-function(y){
+    x<<-y
+    i<<-NULL
+  }
+  
+  get<-function()x
+  
+  setinverse<-function(inverse)i<<-inverse
+  
+  getinverse<-function()i
+  
+  list (set=set ,
+        get=get,
+        setinverse=setinverse,
+        getinverse=getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+## The function creates a inverse matrix . If the matrix data is same then the chached inverse Matrix data is returned
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  
+    i<-x$getinverse()
+    
+    if(!is.null(i)){
+      message("Getting Cached Data")
+      return(i)
+    }
+    data<-x$get()
+    i<-solve(data,...)
+    x$setinverse(i)
+  }
